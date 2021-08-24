8d8d350a054970875a9220ace0864d9192e40873
mzrithm <74382959+mzrithm@users.noreply.github.com>
2021-08-23 21:46:43 -0700

    Added high_score tracking to score.py

--- score.py
+++ score.py
@@ -6,6 +6,7 @@ class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.value = 0
+        self.high_score = 0
         self.pencolor("white")
         self.hideturtle()
         self.broadcast()
@@ -16,7 +17,13 @@ class Score(Turtle):
     def broadcast(self):
         self.clear()
         self.penup()
-        message = f"Score: {self.value}"
+        score_message = f"Score: {self.value} High score: {self.high_score}"
         self.setposition(0, 260)
         self.pendown()
-        self.write(message, False, align="center", font=("Arial", 20, "normal"))
+        self.write(score_message, False, align="center", font=("Arial", 20, "normal"))
+
+    def reset(self):
+        if self.value > self.high_score:
+            self.high_score = self.value
+        self.value = 0
+
